5b164246b12664796f31cea558a120b833791ca8
LuisNeil <lntejedal@gmail.com>
2022-01-19 21:35:04 -0500

    Repositorio de estudio de matrices

---
+++ src/EjemploMatricesStringFor2.java
@@ -0,0 +1,13 @@
+public class EjemploMatricesStringFor2 {
+    public static void main(String[] args) {
+
+        String[][] nombres = {{"Pepe","Pepa"},{"Josefa", "Paco"},{"Lucas","Pancha"}};
+
+        System.out.println("Iterando con for each");
+        for(String[] fila: nombres){
+            for(String nombre: fila){
+                System.out.print(nombre + "\t");
+            }
+        }
+    }
+}
